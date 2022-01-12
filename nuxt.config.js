87e4b4dfc319f64dc32fa4ecac9d194e7eecef21
Felix Corke <felix@blackspike.com>
2022-01-12 14:31:54 +0000

    added vue-agile

--- nuxt.config.js
+++ nuxt.config.js
@@ -48,7 +48,10 @@ export default {
 
   // css: ['@/assets/css/main.scss'],
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [{ src: '~/plugins/vendor/vue-draggable.js' }],
+  plugins: [
+    { src: '~/plugins/vendor/vue-draggable.js' },
+    { src: '~/plugins/vendor/vue-agile' },
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
