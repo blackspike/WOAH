351740ee79aeed9d27dfec290c1d10efaf2c89bb
Felix Corke <felix@blackspike.com>
2022-01-24 14:24:14 +0000

    lite-yt!

--- pages/videos.vue
+++ pages/videos.vue
@@ -1,40 +1,32 @@
 <template lang="pug">
 section.videos
   //- Vid
-  article.videos__wrap
-    h2.videos__title.videos__title--warmup Warmup tutorial
-    .video
-      iframe.video-iframe(
-        src='https://www.youtube.com/embed/qQ96oXp5RTU',
-        title='Warmup tutorial on YouTube',
-        frameborder='0',
-        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture',
-        allowfullscreen
-      )
+  article.video
+    h2.video__title.video__title--warmup Warmup tutorial
+
+    lite-youtube.video__iframe(
+      videoid='qQ96oXp5RTU',
+      playlabel='NerdFitness.com Warmup tutorial',
+      params='rel=0&enablejsapi=1'
+    )
 
   //- Vid
-  article.videos__wrap
-    h2.videos__title.videos__title--workout Workout tutorial
-    .video
-      iframe.video-iframe(
-        src='https://www.youtube.com/embed/qvhHhDNjtxM',
-        title='Workout tutorial on YouTube',
-        frameborder='0',
-        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture',
-        allowfullscreen
-      )
+  article.video
+    h2.video__title.video__title--workout Workout tutorial
+    lite-youtube.video__iframe(
+      videoid='qvhHhDNjtxM',
+      playlabel='NerdFitness.com Workout tutorial',
+      params='rel=0&enablejsapi=1'
+    )
 
   //- Vid
-  article.videos__wrap
-    h2.videos__title.videos__title--neck Neck exercises
-    .video
-      iframe.video-iframe(
-        src='https://www.youtube.com/embed/fTUKGwC_-WE',
-        title='Neck exercises on YouTube',
-        frameborder='0',
-        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture',
-        allowfullscreen
-      )
+  article.video
+    h2.video__title.video__title--neck Neck exercises
+    lite-youtube.video__iframe(
+      videoid='fTUKGwC_-WE',
+      playlabel='Neck exercises on YouTube',
+      params='rel=0&enablejsapi=1'
+    )
 </template>
 
 <script>
@@ -57,6 +49,10 @@ export default {
   max-width: var(--mw-content-wide);
   padding: 0 var(--m) var(--m-xl);
   width: 100%;
+}
+
+.video {
+  width: 100%;
 
   &__title {
     font-size: var(--fs-xl);
@@ -72,15 +68,16 @@ export default {
       color: var(--brand-green);
     }
   }
-}
 
-.video-iframe {
-  aspect-ratio: 16 / 9;
-  background-color: var(--gray-10);
-  border-radius: var(--radius-3);
-  box-shadow: var(--bxs-md);
-  height: auto;
-  overflow: hidden;
-  width: 100%;
+  &__iframe {
+    max-width: none;
+    aspect-ratio: 16 / 9;
+    background-color: var(--gray-10);
+    border-radius: var(--radius-3);
+    box-shadow: var(--bxs-md);
+    height: auto;
+    overflow: hidden;
+    width: 100%;
+  }
 }
 </style>
---
+++ plugins/vendor/youtube-lite.client.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import LiteYoutube from'lite-youtube-embed'
+
+Vue.use(LiteYoutube)
